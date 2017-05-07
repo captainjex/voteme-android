5baa4494da5dc78cc6af88d8de829ce6681f706d
wkwksama <mailpakgasing@gmail.com>
2017-05-08 01:14:36 +0700

    initial commit. baru bikin activity, belum bisa disebut jadi

---
+++ app/src/main/java/com/wkwksama/voteme/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.wkwksama.voteme;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    protected class BtnLoginListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            Intent iLogin = new Intent(v.getContext(),MainActivity.class);
+            startActivity(iLogin);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button btnLogin = (Button)this.findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new BtnLoginListener());
+    }
+}
