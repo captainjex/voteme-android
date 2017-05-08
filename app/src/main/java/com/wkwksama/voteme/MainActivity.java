30111c8f1817f466c1c40d8484285ec58c8cf2fe
wkwksama <mailpakgasing@gmail.com>
2017-05-08 14:15:57 +0700

    nambah menu atas dan pilihan vote di activity main

--- app/src/main/java/com/wkwksama/voteme/MainActivity.java
+++ app/src/main/java/com/wkwksama/voteme/MainActivity.java
@@ -1,29 +1,41 @@
 package com.wkwksama.voteme;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected class VoteListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            Intent iLogin = new Intent(v.getContext(),TempActivity.class);
+            startActivity(iLogin);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Belum dibikin", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+        LinearLayout pilVote1 = (LinearLayout) this.findViewById(R.id.main_pil1);
+        pilVote1.setOnClickListener(new MainActivity.VoteListener());
     }
 
 }
